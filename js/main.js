00a3d038ab80de9e1239a9d984aecfa8b0c09e00
Alanna Tai <alanna.tai@hotmail.com>
2019-11-28 11:58:43 -0500

    Add skill on keypress enter

--- js/main.js
+++ js/main.js
@@ -6,20 +6,30 @@ console.log(localStorage);
 
 //ADD SKILL
 
-$('#add-skill-button').on('click', function() {
+function addSkill() {
 	const $skill = $('#input-skill').val();
-
+	const $newSkill = $(
+		`<li><button id="delete-skill">X</button><span> ${$skill}</span></li>`
+	);
 	if ($skill) {
-		const $newSkill = $(
-			`<li><button id="delete-skill">X</button><span> ${$skill}</span></li>`
-		);
-
 		$('#skills-list').append($newSkill); //append skill to list
 
 		localStorage.setItem('skills', $('#skills-list').html()); //set skill list to storage
 		console.log(localStorage);
 
 		$('#input-skill').val(''); //reset input
+	}
+}
+
+//EVENT LISTENERS
+
+$('#add-skill-button').on('click', function() {
+	addSkill();
+});
+
+$('#input-skill').on('keypress', function(event) {
+	if (event.which === 13) { //on enter, add skill
+		addSkill();
 	}
 });
 
@@ -29,7 +39,7 @@ $('#skills-list').on('click', 'button', function() {
 	$(this)
 		.closest('li')
 		.fadeOut(500, function() {
-			$(this).remove();  //remove skill from DOM
+			$(this).remove(); //remove skill from DOM
 			localStorage.setItem('skills', $('#skills-list').html()); //set current list to storage
 			console.log(localStorage);
 		});
