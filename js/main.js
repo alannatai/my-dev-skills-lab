7bd1ea83bb3121f62bf5c357d825eddc5097b69d
Alanna Tai <alanna.tai@hotmail.com>
2019-11-27 23:01:08 -0500

    finish dev-skills-lab with localStorage

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-$('#add-skill-button')
+//GET STORAGE
+
+const getSkills = localStorage.getItem('skills');
+$('#skills-list').html(getSkills);
+console.log(localStorage);
+
+//ADD SKILL
+
+$('#add-skill-button').on('click', function() {
+	const $skill = $('#input-skill').val();
+
+	if ($skill) {
+		const $newSkill = $(
+			`<li><button id="delete-skill">X</button><span> ${$skill}</span></li>`
+		);
+
+		$('#skills-list').append($newSkill); //append skill to list
+
+		localStorage.setItem('skills', $('#skills-list').html()); //set skill list to storage
+		console.log(localStorage);
+
+		$('#input-skill').val(''); //reset input
+	}
+});
+
+//DELETE LIST ITEM
+
+$('#skills-list').on('click', 'button', function() {
+	$(this)
+		.closest('li')
+		.fadeOut(500, function() {
+			$(this).remove();  //remove skill from DOM
+			localStorage.setItem('skills', $('#skills-list').html()); //set current list to storage
+			console.log(localStorage);
+		});
+});
